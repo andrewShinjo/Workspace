1833d31676c3f2fe1415895ac61c1e28022d7b15
Andrew Shinjo <shinjo1233@gmail.com>
2026-05-09 21:48:49 -0700

    save

---
+++ SwiftUI_Mysticbook/SwiftUI_Mysticbook/Panel/TabItem.swift
@@ -0,0 +1,13 @@
+//
+//  TabItem.swift
+//  SwiftUI_Mysticbook
+//
+//  Created by Andrew Shinjo on 5/9/26.
+//
+
+import Foundation
+
+struct TabItem: Identifiable, Equatable {
+    let id: UUID
+    var title: String
+}
